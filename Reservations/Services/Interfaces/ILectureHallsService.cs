b8a5cf0451ef8a37ef0cff8e19c34084db630a9a
Southern <trphuongnam@hotmail.com>
2017-09-08 22:00:26 +0700

    Initialize

---
+++ Reservations/Services/Interfaces/ILectureHallsService.cs
@@ -0,0 +1,13 @@
+﻿namespace Reservations.Services
+{
+    using System.Collections.Generic;
+    using Models;
+
+    /// <summary>
+    /// Interface for business logic methods concerning lecture halls
+    /// </summary>
+    public interface ILectureHallsService
+    {
+        IEnumerable<LectureHallItem> All();
+    }
+}
---
+++ Reservations/Services/Interfaces/ISubjectsService.cs
@@ -0,0 +1,13 @@
+﻿namespace Reservations.Services
+{
+    using System.Collections.Generic;
+    using Models;
+
+    /// <summary>
+    /// Interface for business logic methods concerning subjects
+    /// </summary>
+    public interface ISubjectsService
+    {
+        IEnumerable<SubjectItem> All();
+    }
+}
